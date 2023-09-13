415c82432b03e468df2d7db7f1c2ba37fac7bde9
IEONIDIC <lvravdin@edu.hse.ru>
2023-09-13 14:20:21 +0300

    Практика #спринт3

---
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/ContactsOrderTypeUi.java
@@ -0,0 +1,20 @@
+package ru.yandex.practicum.contacts.presentation.base;
+
+abstract public class ContactsOrderTypeUi {
+    protected final String type;
+    protected  final boolean isSelected;
+    public ContactsOrderTypeUi(String type, boolean isSelected){
+        this.type = type;
+        this.isSelected = isSelected;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public boolean isSelected() {
+        return isSelected;
+    }
+
+    public abstract String createLogMessage();
+}
